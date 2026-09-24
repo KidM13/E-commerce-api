887a56f2d874f1100e3701331b694e39862a41ff
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-09-24 16:00:21 +0300

    a cart item model is added

--- store/models.py
+++ store/models.py
@@ -9,3 +9,6 @@ class Product(models.Model):
 class Category(models.Model):
     name=models.CharField()
     Product=models.ForeignKey(models.CASCADE)
+class Cart_item(models.Model):
+    Product=models.ForeignKey(models.CASCADE)
+    quantity=models.IntegerField()
