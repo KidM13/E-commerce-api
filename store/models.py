3d5a2479b5ab2702dbbcffe979ef02c550926713
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-09-24 16:22:39 +0300

    price at purchase field is added to order item model

--- store/models.py
+++ store/models.py
@@ -46,6 +46,10 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
 class Order_item(models.Model):
-    Order=models.ForeignKey(models.CASCADE)
-    Product=models.ForeignKey(models.CASCADE)
-    quantity=models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
