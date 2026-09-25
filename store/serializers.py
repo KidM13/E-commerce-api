964f01af52b079c5c6ada43be1d52b925124f257
Kidus Mulugeta <kmulugeta036@gmail.com>
2026-09-25 21:50:31 +0300

    a category serializer is added

--- store/serializers.py
+++ store/serializers.py
@@ -2,3 +2,7 @@ from rest_framework import serializers
 from .models import Category,Product,Cart,Cart_item,Order,Order_item
 
 class CategorySerializer(serializers.Modelserializer):
+    class Meta:
+        model=Category
+        fields=['name']
+        
